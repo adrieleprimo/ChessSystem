34e85c89a9f5101061205cddce098057e1c34ac0
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-04 20:46:08 -0300

    Tried

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		
+		
+		sc.close();
 
 	}
 
