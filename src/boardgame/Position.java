61a1661308e49d54d744e9f4a3258fa37c21d2e9
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-05 16:08:09 -0300

    Added Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,40 @@
+package boardgame;
+
+public class Position {
+	private Integer row;
+	private Integer column;
+
+	public Position() {
+		super();
+	}
+
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setValues(Integer row, Integer column) {
+
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
---
+++ src/model/entities/BoardException.java
@@ -0,0 +1,9 @@
+package model.entities;
+
+public class BoardException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+		public  BoardException (String msg) {
+			super(msg);
+		}
+}
