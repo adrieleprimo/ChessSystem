b8f4877417b2e0b2c7a7aaf4b56a497d253549ae
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-09 15:59:06 -0300

    Special move - Castling 2

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -90,6 +90,27 @@ public class ChessMatch {
 			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
 		}
+		
+		//specialMove Castling KingSide Rook
+		if(p instanceof King && target.getColumn() == source.getColumn()+2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn()+3);
+			Position targetT = new Position(source.getRow(), source.getColumn()+1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+
+		}
+		
+		//queenSide Rook
+		if(p instanceof King && target.getColumn() == source.getColumn()-2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn()-4);
+			Position targetT = new Position(source.getRow(), source.getColumn()-1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+
+		}
+		
 		return capturedPiece;
 	}
 
@@ -102,6 +123,26 @@ public class ChessMatch {
 			board.placePiece(capturedPiece, target);
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
+		}
+		
+		//specialMove Castling KingSide Rook
+		if(p instanceof King && target.getColumn() == source.getColumn()+2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn()+3);
+			Position targetT = new Position(source.getRow(), source.getColumn()+1);
+			ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+			board.placePiece(rook, sourceT);
+			rook.decreaseMoveCount();
+
+		}
+		
+		//queenSide Rook
+		if(p instanceof King && target.getColumn() == source.getColumn()-2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn()-4);
+			Position targetT = new Position(source.getRow(), source.getColumn()-1);
+			ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+			board.placePiece(rook, sourceT);
+			rook.decreaseMoveCount();
+
 		}
 	}
 
